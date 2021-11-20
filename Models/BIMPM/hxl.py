e2f35b8d42222dc775c26d61e5e1f6e789086288
hxlnwpu <yishengtuwei@foxmail.com>
2021-11-20 21:56:10 +0800

    try print

--- Models/BIMPM/hxl.py
+++ Models/BIMPM/hxl.py
@@ -1,5 +1,5 @@
 #run BiPIM
-from run_BIMPM_model import model_train_validate_test
+from run_BIMPM_model import model_train_validate_test,model_load_test
 import pandas as pd
 from utils import Metric, json2df,csv2df
 import os
@@ -13,8 +13,10 @@ vocab_file = os.path.join(bq_path, "data/rand_word_vocab.txt")
 
 target_dir = os.path.join(bq_path, "output/BIMPM_word_rand/")
 
+model_file = os.path.join(bq_path,"output/BIMPM_word_rand/best.pth.tar")
 
-if __name__ == '__main__':
+
+def train_and_test():
     # 微调并且测试
     model_train_validate_test(train_df = train_df,
                 dev_df = dev_df,
@@ -36,3 +38,23 @@ if __name__ == '__main__':
     
     test_result = pd.read_csv(os.path.join(target_dir, 'test_prediction.csv'))
     Metric(test_df.label, test_result.prediction) 
+
+def test():
+    #使用微调好的模型测试
+    model_load_test(test_df = test_df, 
+                    vocab_file = vocab_file, 
+                    embeddings_file = None, 
+                    pretrained_file =model_file,
+                    test_prediction_dir = target_dir, 
+                    test_prediction_name = 'test_prediction.csv', 
+                    mode = 'word', 
+                    num_labels=2, 
+                    max_length=64, 
+                    gpu_index=0, 
+                    batch_size=128)
+
+
+
+if __name__ == '__main__':
+    test()
+
